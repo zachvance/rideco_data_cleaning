79881bc7f1a288ed800aa398f93ed43f13f4219b
zachvance <77811876+zachvance@users.noreply.github.com>
2021-11-30 18:07:35 -0500

    Refactoring

--- main.py
+++ main.py
@@ -1,36 +1,38 @@
-import glob
 import pandas as pd
-import pathlib as pth
 
-print(pth.Path().absolute())
-mainpath = pth.Path().absolute()
-#mainpath = input(r'Enter the filepath that contains all of the data folders: ')
-hours = r'%s/hours/' % mainpath
-rides = r'%s/ride/' % mainpath
-fares = r'%s/fare/' % mainpath
+# File and cleaning of rides report.
+file = r'C:\Users\ZVance\Documents\code\python\paratransit_report_downloader\ride.csv'
+df = pd.read_csv(file)
+df = df.filter(items=['Ride Date',
+                      'Ride ID',
+                      'Booking ID',
+                      'Status',
+                      'User Display Name',
+                      'Driver Display Name',
+                      'Provider',
+                      'Fleet',
+                      'Direct Duration (sec)',
+                      'Distance in km',
+                      'Payment Method',
+                      'Origin Address',
+                      'Origin Latitude',
+                      'Origin Longitude',
+                      'Destination Address',
+                      'Destination Latitude',
+                      'Destination Longitude',
+                      'Program Name',
+                      'Passenger Types',
+                      ])
+df['Payment Method'] = df['Payment Method'].str.replace('SCTC - ', '')
+#df = df[df['Program Name'] == 'Thorold']
 
+temp1 = df.groupby(['Fleet', 'Program Name'])['Payment Method'].value_counts()
+temp2 = df.groupby(['Fleet', 'Program Name'])['Passenger Types'].value_counts()
+temp3 = df['Fleet'].value_counts()
+temp4 = df.groupby(['Fleet', 'Program Name'])['Status'].value_counts()
+temp5 = df.filter(items=['Distance in km', 'Fleet']).groupby(['Fleet']).sum()
 
-def consolidatefiles(path, file):
-    all_files = glob.glob(path + "/*.csv")
-    li = []
-    for filename in all_files:
-        df = pd.read_csv(filename, index_col=None)
-        li.append(df)
-
-    frame = pd.concat(li, axis=0, ignore_index=True)
-    frame.replace('SCTC - ', '', regex=True, inplace=True)
-    frame.to_csv('%s.csv' % file)
-
-
-ptfile = 'pthours'
-consolidatefiles(hours, ptfile)
-consolidatefiles(rides, 'ptrides')
-consolidatefiles(fares, 'ptfares')
-
-def loopthrough():
-    lspaths = [hours, rides, fares]
-    lsfiles = ['pthours', 'ptrides', 'ptfares']
-    for f, p in zip(lspaths, lsfiles):
-        consolidatefiles(f, p)
-
-#loopthrough()
+temp1.to_csv('payment_methods.csv')
+temp2.to_csv('passenger_types.csv')
+temp4.to_csv('statuses.csv')
+temp5.to_csv('distance_in_km.csv')
