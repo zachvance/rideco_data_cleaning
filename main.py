c77cb7fef4b84624b098b3a0b48f889ebe5d07d9
zachvance <77811876+zachvance@users.noreply.github.com>
2021-11-30 18:45:02 -0500

    Refactoring

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 # File and cleaning of rides report.
-file = r'C:\Users\ZVance\Documents\code\python\paratransit_report_downloader\ride.csv'
-df = pd.read_csv(file)
+rides = r'C:\Users\ZVance\Documents\code\python\paratransit_report_downloader\ride.csv'
+hours = r'C:\Users\ZVance\Documents\code\python\paratransit_report_downloader\vehicle_hours.csv'
+df = pd.read_csv(rides)
 df = df.filter(items=['Ride Date',
                       'Ride ID',
                       'Booking ID',
@@ -36,3 +37,38 @@ temp1.to_csv('payment_methods.csv')
 temp2.to_csv('passenger_types.csv')
 temp4.to_csv('statuses.csv')
 temp5.to_csv('distance_in_km.csv')
+
+hours = pd.read_csv(hours)
+hours = hours.filter(items=['Program Name',
+                            'Agenda Day',
+                            'Booking ID',
+                            'Driver Email',
+                            'Online Time (minutes)',
+                            'Offline Time (minutes)',
+                            'Working Time (minutes)',
+                            'Trips Offered',
+                            'Trips Accepted',
+                            'Trips Ignored',
+                            'Trips Rejected',
+                            ])
+
+temp = df.filter(items=['Booking ID', 'Fleet'])
+hours = hours.merge(temp, on='Booking ID', how='left')
+hours.to_csv('hours_debug.csv')
+
+temp1 = hours.filter(items=['Online Time (minutes)',
+                            'Offline Time (minutes)',
+                            'Working Time (minutes)',
+                            'Fleet',
+                            'Program Name',
+                            ]).groupby(['Fleet', 'Program Name']).sum()
+temp2 = hours.filter(items=['Trips Offered',
+                            'Trips Accepted',
+                            'Trips Ignored',
+                            'Trips Rejected',
+                            'Fleet',
+                            'Program Name',
+                            ]).groupby(['Fleet', 'Program Name']).sum()
+
+temp1.to_csv('hours.csv')
+temp2.to_csv('accepted_ignored.csv')
