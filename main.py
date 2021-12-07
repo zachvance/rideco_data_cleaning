665e05a491bdaf8c3aa979f0bde330d1c050d3ab
zachvance <zachariah.vance@live.com>
2021-12-07 09:11:10 -0500

    Refactoring

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from config import FLEET_GROUPS, PASSENGER_GROUPS
 
 # File and cleaning of rides report.
 rides = r'C:\Users\ZVance\Documents\code\python\paratransit_report_downloader\ride.csv'
@@ -23,15 +26,25 @@ df = df.filter(items=['Ride Date',
                       'Destination Longitude',
                       'Program Name',
                       'Passenger Types',
+                      'Actual/Estimated Pickup Time',
                       ])
 df['Payment Method'] = df['Payment Method'].str.replace('SCTC - ', '')
-#df = df[df['Program Name'] == 'Thorold']
+df['Payment Method'] = df['Payment Method'].str.replace('/Commission Member', '')
 
-temp1 = df.groupby(['Fleet', 'Program Name'])['Payment Method'].value_counts()
-temp2 = df.groupby(['Fleet', 'Program Name'])['Passenger Types'].value_counts()
-temp3 = df['Fleet'].value_counts()
-temp4 = df.groupby(['Fleet', 'Program Name'])['Status'].value_counts()
-temp5 = df.filter(items=['Distance in km', 'Fleet']).groupby(['Fleet']).sum()
+df['Group'] = df['Fleet'].map(FLEET_GROUPS)
+df['Passenger Groups'] = df['Passenger Types'].map(PASSENGER_GROUPS)
+
+#temp1 = df.groupby(['Group', 'Fleet', 'Program Name'])['Payment Method'].value_counts()
+temp1 = df.filter(['Group', 'Fleet', 'Program Name', 'Payment Method'])
+
+#temp2 = df.groupby(['Group', 'Fleet', 'Program Name'])['Passenger Groups'].value_counts()
+temp2 = df.filter(['Group', 'Fleet', 'Program Name', 'Passenger Groups'])
+temp3 = df['Group'].value_counts()
+#temp4 = df.groupby(['Group', 'Fleet', 'Program Name'])['Status'].value_counts()
+temp4 = df.filter(items=['Group', 'Fleet', 'Program Name', 'Status'])
+temp5 = df.filter(items=['Distance in km', 'Group', 'Fleet', 'Program Name'])
+#[~df['Status'].isin(['Abandoned'])]
+
 
 temp1.to_csv('payment_methods.csv')
 temp2.to_csv('passenger_types.csv')
@@ -52,23 +65,46 @@ hours = hours.filter(items=['Program Name',
                             'Trips Rejected',
                             ])
 
-temp = df.filter(items=['Booking ID', 'Fleet'])
-hours = hours.merge(temp, on='Booking ID', how='left')
-hours.to_csv('hours_debug.csv')
+
+temp = df.filter(['Booking ID', 'Fleet'])
+#hours = hours.merge(temp, on='Booking ID', how='right')
+
+
+#hours['Group'] = hours['Fleet'].map(FLEET_GROUPS)
 
 temp1 = hours.filter(items=['Online Time (minutes)',
                             'Offline Time (minutes)',
                             'Working Time (minutes)',
-                            'Fleet',
                             'Program Name',
-                            ]).groupby(['Fleet', 'Program Name']).sum()
+                            ])
+temp1['Online Hours'] = temp1['Online Time (minutes)'] / 60
+temp1['Offline Hours'] = temp1['Offline Time (minutes)'] / 60
+temp1['Working Hours'] = temp1['Working Time (minutes)'] / 60
+
+temp1 = temp1.filter(items=['Online Hours',
+                            'Offline Hours',
+                            'Working Hours',
+                            'Program Name',
+                            ])
+#temp1 = temp1.groupby(['Fleet', 'Group', 'Program Name']).sum()
+
 temp2 = hours.filter(items=['Trips Offered',
                             'Trips Accepted',
                             'Trips Ignored',
                             'Trips Rejected',
                             'Fleet',
+                            'Group',
                             'Program Name',
-                            ]).groupby(['Fleet', 'Program Name']).sum()
+                            ])
 
 temp1.to_csv('hours.csv')
 temp2.to_csv('accepted_ignored.csv')
+
+data = df.filter(items=['Actual/Estimated Pickup Time', 'Direct Duration (sec)'])
+data = data.dropna()
+data['Hour of Day'] = pd.to_datetime(data['Actual/Estimated Pickup Time'])
+data['Hour of Day'] = data['Hour of Day'].dt.strftime('%H')
+data['Hour of Day'] = data['Hour of Day'].astype(int)
+
+sns.lineplot(x='Hour of Day', y='Direct Duration (sec)', data=data)
+plt.show()
